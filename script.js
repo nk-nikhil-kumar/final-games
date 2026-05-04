06a686656c395a5d45d78f162369c32606d9014a
Nikhil <nk9138812@gmail.com>
2026-05-04 23:12:08 +0530

    change

--- script.js
+++ script.js
@@ -86,3 +86,10 @@ const revealObserver = new IntersectionObserver((entries) => {
 document.querySelectorAll(".reveal").forEach(el => {
   revealObserver.observe(el);
 });
+
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-03931ZW0LV');
